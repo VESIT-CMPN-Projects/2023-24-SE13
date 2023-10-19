dd61e44a2231eb13c800b0e1992634ac2b6fe22b
Aadilshah786 <adilns786@gmail.com>
2023-10-19 23:56:02 +0530

    firebase data collection

--- AirAware/script_ll.js
+++ AirAware/script_ll.js
@@ -1,9 +1,15 @@
-// var submit = document.getElementById("submit");
-// submit.addEventListener("click", () => {
-//   callit();
+var cityin = document.getElementById("cityin");
+var search = document.getElementById("search-button");
+search.addEventListener("click", () => {
+  event.preventDefault();
+  // searchit();
+  fetchDataForCity(cityin.value);
 
-//   console.log("button clicked");
-// });
+  console.log("search button clicked");
+});
+
+
+
 
 var selector ="t";
 
@@ -161,6 +167,27 @@ const cities = [
 ];
 
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+import { getFirestore, collection, getDocs, doc, setDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+// Initialize Firebase with your project's configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1AC3ptMI60oGbYBeseroCTdnEDex7AQg",
+  authDomain: "airaware-f22c0.firebaseapp.com",
+  projectId: "airaware-f22c0",
+  storageBucket: "airaware-f22c0.appspot.com",
+  messagingSenderId: "580535713947",
+  appId: "1:580535713947:web:010ec1671df4d20a1c863e",
+  measurementId: "G-RQ1WWWJWKP"
+};
+// firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+
+const collectionRef = collection(db, "data");
+
 
 function fetchDataloop(){
   for(let i=0; i<cities.length ; i++ ){
@@ -177,7 +204,42 @@ function fetchDataForCity(city) {
       .then(function(data) {
         
         updateJsonWithData(data,city); //calling function to update json data
-        //  console.log(data);
+          console.log(data);
+
+          
+          var firedata = {
+            "type": "Feature",
+            "geometry": {
+            "type": "Point",
+            "coordinates": [data.data.city.geo[1],data.data.city.geo[0]]//data.location.lon, data.location.lat
+              },
+            "properties": {
+            "id":i,
+            "name": `${city}`, //data.location.name,
+            "aqi":data.data.aqi ,
+            "t": data.data.iaqi.t.v,
+            "h": data.data.iaqi.h.v,
+            }
+          };
+           const customDocId = city; // Replace with your desired custom ID
+
+          const customDocRef = doc(collectionRef, customDocId);
+          
+          
+          setDoc(customDocRef, firedata)
+            .then(() => {
+              console.log("Custom document added with ID: " + customDocId);
+            })
+            .catch((error) => {
+              console.error("Error adding custom document: ", error);
+            });
+          
+            const newData = {
+            "properties.t": 0, // Update the "t" field
+            "properties.h": 2, // Update the "h" field
+            "properties.aqi": 3, // Update the "aqi" field
+          };
+
           return data; // Return the data from the API
       })
       .catch(function(error) {
