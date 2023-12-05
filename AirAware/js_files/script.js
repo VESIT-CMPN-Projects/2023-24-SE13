150180b8f9e80117cf54a563def1728264adb260
AadilShah786 <adilns786@.com>
2023-12-06 00:01:54 +0530

    major change 2

--- AirAware/js_files/script.js
+++ AirAware/js_files/script.js
@@ -14,6 +14,15 @@ apidata()
     updateGovData(data);
   })
 
+  async () => {
+    try {
+      const response = await fetch('YOUR_FIREBASE_FUNCTION_ENDPOINT');
+      const data = await response.json();
+      console.log(data.message); // This will log the response from the function
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   var search = document.getElementById("search-button");
 
@@ -77,64 +86,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var geojsonLayer = L.geoJSON().addTo(map);
 
 
-// Custom Marker at Delhi's coordinates
-
-// Adding a popup to the marker
-
-// var marker = L.marker(coordinates).addTo(map);
-//   //   marker.bindPopup(`City: ${name}<br>CO: ${co}`).openPopup();
-//   //
-//   layer.bindPopup(`City: ${name}<br>CO: ${co}`).openPopup();
-if (!jsongovdataarray || !jsongovdataarray.length) {
-  jsongovdataarray = coordata;
-}
-function  updateMapWithData (){
-  jsongovdataarray.map(feature => {
-
-    function customIcon(col){
-      var color;
-      if(col=="nh3"){
-        color ="green.jpg";}
-        if(col=="co"){
-          color ="blue.jpg";}
-          if(col=="so2"){
-            color ="yellow.jpg";}
-            if(col=="o3"){
-              color ="orange.png";}
-              if(col=="no2"){
-                color ="Red.png";}
-                if(col=="pm10"){
-                  color ="purple.jpg";}
-                  if(col=="pm25"){
-                    color ="black.jpg";}
-    return  customIcon = L.icon({
-      iconUrl: `./ImagesOfSite/${color}`,
-      iconSize: [10, 10], // size of the icon
-      iconAnchor: [5, 5], // point of the icon which will correspond to marker's location
-      popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
-    });}
-    try{
-    var marker = L.marker([feature.geometry.coordinates[0], // Latitude
-    feature.geometry.coordinates[1],], { icon: customIcon(feature.properties.maxele) });
-     marker.addTo(map);
-    marker.bindPopup(`<b>${feature.properties.station}</b>
-                    <br><b>City: ${feature.properties.city}</b>
-                    <br>Aqi: ${feature.properties.aqi} ug/m3
-                    <br>PM 2.5: ${feature.properties.Pm25} ug/m3
-                    <br>PM 10: ${feature.properties.Pm10} ug/m3
-                    <br>NO2: ${feature.properties.No2} ug/m3
-                    <br>O3: ${feature.properties.Ozone} ug/m3
-                    <br>SO2: ${feature.properties.So2} ug/m3
-                    <br>CO: ${feature.properties.Co} mg/m3
-                    <br>NH3: ${feature.properties.Nh3} ug/m3
-                    `).openPopup();
-    }catch(error){
-      console.log(error);
-    }
-    // Adding a popup to the marker
-  });
-
-}
 var note = L.control({ position: 'bottomright' });
 
 note.onAdd = function () {
@@ -147,16 +98,17 @@ var legend = L.control({ position: 'bottomright' });
 legend.onAdd = function () {
   var div = L.DomUtil.create('div', 'info legend');
   div.innerHTML = '<h4>Most dominant pollutant</h4>' +
-    '<div class="legend-item"><div class="legend-color" style="background: black"></div> pm25 </div>' +
+    '<div class="legend-item"><div class="legend-color" style="background: black"></div> pm2.5 </div>' +
     '<div class="legend-item"><div class="legend-color" style="background: purple"></div> pm10 </div>' +
     '<div class="legend-item"><div class="legend-color" style="background: red"></div> no2 </div>' +
     '<div class="legend-item"><div class="legend-color" style="background: orange"></div> ozone </div>' +
     '<div class="legend-item"><div class="legend-color" style="background: yellow"></div> so2 </div>' +
     '<div class="legend-item"><div class="legend-color" style="background: blue"></div> co </div>' +
-    '<div class="legend-item"><div class="legend-color" style="background: green"></div> nh3 </div>';
+    '<div class="legend-item"><div class="legend-color" style="background: lightgreen"></div> nh3 </div>';
 
   return div;
 };
+console.log("lol")
 note.addTo(map);
 legend.addTo(map);
 
@@ -164,6 +116,77 @@ legend.addTo(map);
 document.querySelector('.legend').style.marginRight = '10px'; // Adjust the margins as needed
 document.querySelector('.note').style.marginTop = '10px'; // Adjust the margins as needed
 
+// Custom Marker at Delhi's coordinates
+
+// Adding a popup to the marker
+
+// var marker = L.marker(coordinates).addTo(map);
+//   //   marker.bindPopup(`City: ${name}<br>CO: ${co}`).openPopup();
+//   //
+//   layer.bindPopup(`City: ${name}<br>CO: ${co}`).openPopup();
+// if (!jsongovdataarray || !jsongovdataarray.length) {
+//   jsongovdataarray = coordata;
+// }
+function  updateMapWithData (){
+  jsongovdataarray.map(feature => {
+
+    function customIcon(col,val){
+      var color;
+      if(col=="nh3"){
+        color ="green.png";}
+        else if(col=="co"){
+          color ="blue.png";}
+          else if(col=="so2"){
+            color ="yellow.png";}
+            else if(col=="o3"){
+              color ="orange.png";}
+              else if(col=="no2"){
+                color ="Red.png";}
+                else if(col=="pm10"){
+                  color ="purple.png";}
+                  else if(col=="pm25"){
+                    color ="black.png";}
+                    else{color="grey.png"}
+    return  customIcon = L.icon({
+      iconUrl: `./ImagesOfSite/${color}`,
+    //   html: `<div style=" height:20px;width:20px; position:absolute;">
+    //   <img src="./ImagesOfSite/${color}"  style="width:16px; height:16px; padding: 0;border: none;">
+    //   <div style="">${val}</div>
+    // </div>
+    // `,
+      iconSize: [10, 10], // size of the icon
+      iconAnchor: [5, 5], // point of the icon which will correspond to marker's location
+      popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+  }
+    try{
+    var marker = L.marker([feature.geometry.coordinates[0], // Latitude
+    feature.geometry.coordinates[1],], { icon: customIcon(feature.properties.maxele, feature.properties.max) });
+     marker.addTo(map);
+     let aqi =feature.properties.aqi;
+     if (aqi==-1){
+      aqi="Insufficient data";
+     } else{
+      aqi =aqi + " ug/m3";
+     }
+    marker.bindPopup(`<b>${feature.properties.station}</b>
+                    <br><b>City: ${feature.properties.city}</b>
+                    <br>Aqi: ${aqi}
+                    <br>PM 2.5: ${feature.properties.Pm25} ug/m3
+                    <br>PM 10: ${feature.properties.Pm10} ug/m3
+                    <br>NO2: ${feature.properties.No2} ug/m3
+                    <br>O3: ${feature.properties.Ozone} ug/m3
+                    <br>SO2: ${feature.properties.So2} ug/m3
+                    <br>CO: ${feature.properties.Co} mg/m3
+                    <br>NH3: ${feature.properties.Nh3} ug/m3
+                    `);
+    }catch(error){
+      console.log(error);
+    }
+    // Adding a popup to the marker
+  });
+
+}
 
 updateMapWithData();
 export { updateMapWithData };
